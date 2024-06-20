544a5bd6e56c3b271780dfc3f1fdd79eee4937fa
SergeiIonin <sai1042@yandex.ru>
2024-06-20 19:10:38 -0400

    add srclient.Schema

--- kafka_aggregator/test/SchemaServiceTest.go
+++ kafka_aggregator/test/SchemaServiceTest.go
@@ -1,31 +1,111 @@
 package test
 
-import kafkaAggregator "kafka_updates_aggregator/kafka_aggregator"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/riferrei/srclient"
+)
 
 type SchemaServiceTest struct {
-	FieldToSchemas    map[string][]kafkaAggregator.Schema
-	FieldToNamespaces map[string][]string
+	FieldToSchemas     map[string][]*srclient.Schema
+	FieldToNamespaces  map[string][]string
+	SubjectToNamespace map[string]string
+	Subject            string
 }
 
-func NewSchemaServiceTest() *SchemaServiceTest {
+func NewSchemaServiceTest(subject string) *SchemaServiceTest {
 	return &SchemaServiceTest{
-		FieldToSchemas:    map[string][]kafkaAggregator.Schema{},
-		FieldToNamespaces: map[string][]string{},
+		FieldToSchemas:     map[string][]*srclient.Schema{},
+		FieldToNamespaces:  map[string][]string{},
+		SubjectToNamespace: map[string]string{},
+		Subject:            subject,
 	}
 }
 
-func (sst *SchemaServiceTest) CreateSchema(schema kafkaAggregator.Schema, namespace string) error {
-	fields := schema.Fields
+func (sst *SchemaServiceTest) SaveSchema(schema *srclient.Schema, namespace string) error {
+	fields, _ := sst.GetSchemaFields(schema)
+	subject, err := sst.GetSchemaSubject(schema)
+	if err != nil {
+		return fmt.Errorf("could not get subject for schema %v", err)
+	}
 	for _, field := range fields {
 		sst.FieldToSchemas[field] = append(sst.FieldToSchemas[field], schema)
 		sst.FieldToNamespaces[field] = append(sst.FieldToNamespaces[field], namespace)
+		sst.SubjectToNamespace[subject] = namespace
 	}
 	return nil
 }
 
-func (sst *SchemaServiceTest) GetSchemasForField(field string) ([]kafkaAggregator.Schema, error) {
+// todo support avro and protobuf as well
+func (sst *SchemaServiceTest) GetSchemaSubject(schema *srclient.Schema) (string, error) {
+	schemaType := schema.SchemaType()
+	if *schemaType != srclient.Json {
+		return "", fmt.Errorf("unsupported schema type %v", *schemaType)
+	}
+	// if schema has multiple References then they can be different in only Version (Subject and Name should be the same)
+	// tests only
+	subject := sst.Subject
+	//subject := schema.References()[0].Subject
+	return subject, nil
+}
+
+func (sst *SchemaServiceTest) GetSchemasForField(field string) ([]*srclient.Schema, error) {
 	return sst.FieldToSchemas[field], nil
 }
+
+type FieldRepr struct {
+	Name string
+	Type string
+}
+
+type SchemaRepr struct {
+	Type   string
+	Name   string
+	Fields []FieldRepr
+}
+
+func (sst *SchemaServiceTest) GetSchemaFields(schema *srclient.Schema) ([]string, error) {
+	schemaType := schema.SchemaType()
+	if *schemaType != srclient.Json {
+		return nil, fmt.Errorf("unsupported schema type %v", *schemaType)
+	}
+	//res := make(map[string]any)
+	res := SchemaRepr{}
+	err := json.Unmarshal([]byte(schema.Schema()), &res)
+	if err != nil {
+		return []string{}, fmt.Errorf("could not unmarshal schema %v", err)
+	}
+	fields := make([]string, 0, len(res.Fields))
+	for _, field := range res.Fields {
+		fields = append(fields, field.Name)
+	}
+	return fields, nil
+}
+
 func (sst *SchemaServiceTest) GetNamespacesForField(field string) ([]string, error) {
 	return sst.FieldToNamespaces[field], nil
 }
+
+func (sst *SchemaServiceTest) GetNamespaceBySubject(subject string) (string, error) {
+	return sst.SubjectToNamespace[subject], nil
+}
+
+/*func (sst *SchemaServiceTest) GetNamespacesForField(field string) ([]string, error) {
+	schemasForField, err := sst.GetSchemasForField(field)
+	if err != nil {
+		return nil, fmt.Errorf("could not get schemas for field %s, %v", field, err)
+	}
+	namespacesForField := make([]string, 0, len(schemasForField))
+	for _, schema := range schemasForField {
+		subject, err := sst.GetSchemaSubject(schema)
+		if err != nil {
+			return nil, fmt.Errorf("could not get subject for schema %v", err)
+		}
+		namespace, err := sst.GetNamespaceBySubject(subject)
+		if err != nil {
+			return nil, fmt.Errorf("could not get namespace for subject %v", err)
+		}
+		namespacesForField = append(namespacesForField, namespace)
+	}
+	return namespacesForField, nil
+}*/
--- kafka_aggregator/test/TestKafkaAggregator_test.go
+++ kafka_aggregator/test/TestKafkaAggregator_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/docker/docker/client"
+	"github.com/riferrei/srclient"
 	"github.com/segmentio/kafka-go"
 	"kafka_updates_aggregator/kafka_aggregator"
 	"kafka_updates_aggregator/testutils"
@@ -12,14 +13,15 @@ import (
 )
 
 var (
-	kafkaBroker       = "localhost:9092"
-	kafkaAddr         = kafka.TCP(kafkaBroker)
-	mergedSourceTopic = "test_merged"
-	aggrTopic         = "user_balance_updates"
-	kafka_client      *kafka.Client
-	containerId       string
-	err               error
-	dockerClient      *client.Client
+	kafkaBroker          = "localhost:9092"
+	kafkaAddr            = kafka.TCP(kafkaBroker)
+	mergedSourceTopic    = "test_merged"
+	aggregateTopic       = "user_balance_updates"
+	kafka_client         *kafka.Client
+	containerId          string
+	err                  error
+	dockerClient         *client.Client
+	schemaRegistryClient srclient.ISchemaRegistryClient
 )
 
 func init() {
@@ -40,7 +42,7 @@ func init() {
 		Transport: nil,
 	}
 
-	allTopics := []string{aggrTopic, mergedSourceTopic}
+	allTopics := []string{aggregateTopic, mergedSourceTopic}
 
 	topicConfigs := make([]kafka.TopicConfig, len(allTopics))
 	for i, topic := range allTopics {
@@ -59,18 +61,20 @@ func init() {
 	); err != nil {
 		log.Fatalf("could not create topics %v", err)
 	}
+
+	schemaRegistryClient = srclient.CreateMockSchemaRegistryClient("http://localhost:8081")
 }
 
 func TestKafkaAggregator_test(t *testing.T) {
-	cleanup := func() {
+	/*cleanup := func() {
 		testutils.CleanupAndGracefulShutdown(t, dockerClient, containerId)
 	}
 
 	//defer cleanup() // fixme it'd be great to rm containers in case t.Cleanup won't affect them
-	t.Cleanup(cleanup)
+	t.Cleanup(cleanup)*/
 
 	cache := NewCacheTest()
-	schemaService := NewSchemaServiceTest()
+	schemaService := NewSchemaServiceTest(aggregateTopic)
 	kafkaReader := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:  []string{kafkaBroker},
 		Topic:    mergedSourceTopic,
@@ -88,15 +92,32 @@ func TestKafkaAggregator_test(t *testing.T) {
 		cache,
 	}
 
-	schema := kafka_aggregator.Schema{
-		"schema_users_1",
-		"users",
-		aggrTopic,
-		[]string{"user_id", "balance", "deposit", "withdrawal"},
-	}
+	schemaRaw := `{
+		"type": "record",
+		"name": "user_balance_updates",
+		"fields": [
+			{"name": "user_id", "type": "string"},
+			{"name": "balance", "type": "int"},
+			{"name": "deposit", "type": "int"},
+			{"name": "withdrawal", "type": "int"}
+		]
+	}`
+
+	schema, err := schemaRegistryClient.CreateSchema(aggregateTopic, schemaRaw, srclient.Json, srclient.Reference{
+		Name:    aggregateTopic,
+		Subject: aggregateTopic,
+		Version: 0,
+	})
+	refs := schema.References()
+	refs = append(refs, srclient.Reference{
+		Name:    aggregateTopic,
+		Subject: aggregateTopic,
+		Version: 0,
+	})
 
 	userId := "bob"
-	schemaService.CreateSchema(schema, schema.Namespace)
+	schemaService.SaveSchema(schema, "users")
+
 	cache.CreateNamespace("users")
 	cache.Create("users", userId, "balance", 1000)
 	cache.Create("users", userId, "deposit", 500)
@@ -105,7 +126,7 @@ func TestKafkaAggregator_test(t *testing.T) {
 	payload := []byte(`{"balance": 1200, "deposit": 700, "isAuthenticated": true, "country": "Cordovia"}`)
 
 	message := kafka.Message{
-		Topic: schema.Topic,
+		Topic: aggregateTopic,
 		Key:   []byte(userId),
 		Value: payload,
 	}
@@ -114,7 +135,7 @@ func TestKafkaAggregator_test(t *testing.T) {
 	testReader := testutils.KafkaTestReader{
 		kafka.NewReader(kafka.ReaderConfig{
 			Brokers:  []string{kafkaBroker},
-			Topic:    aggrTopic,
+			Topic:    aggregateTopic,
 			MinBytes: 10e3, // 10KB
 			MaxBytes: 10e6, // 10MB
 		}),
