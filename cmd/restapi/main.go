f006178f101185ba56afa30f7dde3cc723a7e96d
SergeiIonin <sai1042@yandex.ru>
2024-07-18 16:22:11 -0400

    add config for rest api

--- cmd/restapi/main.go
+++ cmd/restapi/main.go
@@ -1,14 +1,57 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "kafka_updates_aggregator/restapi"
-    "kafka_updates_aggregator/restapi/application"
-    "net/http"
+	"fmt"
+	"kafka_updates_aggregator/configs"
+	"kafka_updates_aggregator/restapi"
+	"kafka_updates_aggregator/restapi/application"
+	"log"
+	"net/http"
+	"os"
+	restapiconfig "kafka_updates_aggregator/configs/restapi"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
 )
 
+var (
+	configTplPath string
+	valuesPath    string
+	configPath    string
+	configFile    []byte
+)
+
+func init() {
+	pwd, _ := os.Getwd()
+	configTplPath = fmt.Sprintf("%s/configs/restapi/templates/restapi_config_template.yaml", pwd)
+	valuesPath = fmt.Sprintf("%s/configs/restapi/values.yaml", pwd)
+	configPath = fmt.Sprintf("%s/configs/restapi/restapi_config.yaml", pwd)
+
+	configReader := configs.NewConfigReader()
+
+	err := configReader.ReadConfig(configTplPath, valuesPath, configPath)
+	if err != nil {
+		log.Fatalf("Error creating restapi_config: %v", err)
+	}
+	configFile, err = os.ReadFile(configPath)
+	if err != nil {
+		log.Fatalf("Error reading restapi config file: %v", err)
+	}
+}
+
 func main() {
-	schemaRegistryUrl := "http://localhost:8081"
+	var restapiConfig restapiconfig.RestApiConfig
+
+	err := yaml.Unmarshal(configFile, &restapiConfig)
+	if err != nil {
+		log.Fatalf("Error parsing restapiConfig file: %v", err)
+	}
+
+	conf := restapiConfig.RestApi
+
+	schemaRegistryUrl := conf.SchemaRegistryAddress
+	address := fmt.Sprintf("%s:%d", conf.Host, conf.Port)
+
 	aggregationSchemasService := application.NewSchemaAggregationService(schemaRegistryUrl)
 
 	rc := restapi.NewRestController(aggregationSchemasService)
@@ -19,7 +62,7 @@ func main() {
 	router.POST("/create", rc.CreateSchema)
 	router.GET("/schemas", rc.GetSchemas)
 
-    err := router.Run("localhost:8082")
+    err = router.Run(address)
     if err != nil {
         panic(err)
     }
