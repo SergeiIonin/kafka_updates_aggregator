aba7f86d9d5df929315c7fce4c0350ffcc5dbc21
SergeiIonin <sai1042@yandex.ru>
2024-09-03 15:42:39 -0400

    [test] fix auxiliary funcs for run with context

--- test/RunWithContext.go
+++ test/RunWithContext.go
@@ -10,24 +10,21 @@ import (
 // base function to control the exeecution of schemas handling, merging, aggregation and reading the aggregations topics
 func RunWithTimeout(t *testing.T, name string, timeout time.Duration, f func(ctx context.Context), wg *sync.WaitGroup) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
 	go f(ctx)
 	go func() {
-		select {
-		case <-ctx.Done():
-			t.Logf("context for %s is canceled after %v", name, timeout)
-			if wg != nil {
-				wg.Done()
-			}
-			cancel()
+		<-ctx.Done()
+		t.Logf("context for %s is canceled after %v", name, timeout)
+		if wg != nil {
+			wg.Done()
 		}
+		cancel()
 	}()
 }
 
 func RunWithContext(ctx context.Context, cancel context.CancelFunc, t *testing.T, name string, f func(ctx context.Context)) {
 	go f(ctx)
-	select {
-	case <-ctx.Done():
-		t.Logf("context for %s is canceled", name)
-		cancel()
-	}
+	<-ctx.Done()
+	t.Logf("context for %s is canceled", name)
+	cancel()
 }
